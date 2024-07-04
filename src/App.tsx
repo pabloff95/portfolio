740eeda44e8b5533f1cbb01ddd9c06b12e8f81e7
Pablo Fernandez <pfernandez@dataahead.de>
2024-07-04 19:49:41 +0200

    Add react-router-dom and link it to the main pages of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+
 function App() {
   return (
-    <>      
-      <p>
-        Hello world
-      </p>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

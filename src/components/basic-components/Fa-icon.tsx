9b4e2e92d3b0f36bdd79531ba9494448e0c5fdd8
Pablo Fernandez <pfernandez@dataahead.de>
2024-07-06 10:29:10 +0200

    Install font awesome and implement FaIcon component

---
+++ src/components/basic-components/Fa-icon.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  IconDefinition,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { library, IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface FaIconProps {
+  icon: string;
+  className?: string;
+  iconColor?: string;
+}
+
+const iconList: IconDefinition[] = [faGithub, faLinkedin];
+
+library.add(...iconList);
+
+const FaIcon: React.FC<FaIconProps> = ({
+  icon,
+  className,
+  iconColor = "var(--text-color)",
+}) => {
+  return (
+    <FontAwesomeIcon
+      icon={icon as IconProp}
+      className={className}
+      style={{ color: iconColor }}
+    />
+  );
+};
+
+export default FaIcon;

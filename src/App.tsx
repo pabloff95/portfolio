e9970b1ed64475edee4ac994516f0d454ce81d6a
Pablo Fernandez <pfernandez@dataahead.de>
2024-07-07 15:47:09 +0200

    Add elmenet is in view obsrever to layout component

--- src/App.tsx
+++ src/App.tsx
@@ -4,42 +4,19 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Layout from "./components/layout/Layout";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    // Add show animation for DOM elments with the hidden-animation class, so that they scroll in to the view port of the user
-    const elmentInViewPortObserver: IntersectionObserver =
-      new IntersectionObserver((domElements) => {
-        domElements.forEach((element) => {
-          if (element.isIntersecting) {
-            // Element entered in the viewport
-            element.target.classList.add("show-animation");
-          }
-        });
-      });
-
-    const hiddenDomElements: Element[] = Array.from(
-      document.getElementsByClassName("hidden-animation")
-    );
-    hiddenDomElements.forEach((element) =>
-      elmentInViewPortObserver.observe(element)
-    );
-  }, []);
-
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Route>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,32 @@
 import SideMenu from "./Side-menu";
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FaIcon from "../basic-components/Fa-icon";
+import { useLocation } from "react-router-dom";
 
 export default function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Add show animation for DOM elments with the hidden-animation class, so that they scroll in to the view port of the user
+    const elmentInViewPortObserver: IntersectionObserver =
+      new IntersectionObserver((domElements) => {
+        domElements.forEach((element) => {
+          if (element.isIntersecting) {
+            // Element entered in the viewport
+            element.target.classList.add("show-animation");
+          }
+        });
+      });
+
+    const hiddenDomElements: Element[] = Array.from(
+      document.getElementsByClassName("hidden-animation")
+    );
+    hiddenDomElements.forEach((element) =>
+      elmentInViewPortObserver.observe(element)
+    );
+  }, [location]);
+
   const [displayMenu, setDisplayMenu] = useState(true);
 
   return (

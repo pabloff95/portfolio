5d23aaa85f8fd735cfeb3c3131a911a81457c951
Pablo Fernandez <pfernandez@dataahead.de>
2024-08-19 18:57:25 +0200

    Make home page responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,32 +2,32 @@ export default function Home() {
   return (
     <>
       <section className="h-[100vh] w-full overflow-hidden">
-        <div className="flex h-full w-full justify-center items-center px-4">
-          <div className="w-[40%] hidden-animation hidden-animation-left">
+        <div className="flex flex-col lg:flex-row h-full w-full justify-center items-center px-4">
+          <div className="mb-6 lg:mb-0 w-[70%] lg:w-[40vw] lg:hidden-animation lg:hidden-animation-left">
             <img
               className="rounded-3xl m-auto"
               src="/images/profile.jpg"
               alt="Profile picture"
             />
           </div>
-          <div className="font-impact text-center w-[60%] px-2 flex flex-col gap-8 hidden-animation hidden-animation-right">
-            <p className="text-5xl leading-snug">
+          <div className="font-impact text-center w-full md:w-[60vw] flex flex-col gap-8 md:hidden-animation md:hidden-animation-right">
+            <p className="text-3xl md:text-4xl xl:text-5xl leading-snug">
               Welcome! <br />I am Pablo, a{" "}
               <span className="text-primary">web developer</span> especiallized
-              in <span className="text-primary">frontend</span> technologies
+              in <span className="text-primary">frontend</span>
             </p>
             <div className="m-auto w-[80%] bg-font-base h-1 rounded-lg"></div>
-            <div className="mt-2 flex gap-12 w-full justify-center">
-              <div className="py-3 px-6 border-2 border-font-base text-lg rounded-md flex items-center justify-center">
+            <div className="mt-2 flex flex-row gap-4 2xl:gap-12 w-full justify-center">
+              <div className="w-fit py-1 lg:py-2 2xl:py-3 px-2 lg:px-4 2xl:px-6 border-2 border-font-base lg:text-md 2xl:text-lg rounded-md flex items-center justify-center">
                 <span>Web development</span>
               </div>
-              <div className="py-3 px-6 border-2 border-font-base text-lg rounded-md flex items-center justify-center">
+              <div className="w-fit py-1 lg:py-2 2xl:py-3 px-2 lg:px-4 2xl:px-6 border-2 border-font-base lg:text-md 2xl:text-lg rounded-md flex items-center justify-center">
                 <span>UI Design</span>
               </div>
-              <div className="py-3 px-6 border-2 border-font-base text-lg rounded-md flex items-center justify-center">
+              <div className="w-fit py-1 lg:py-2 2xl:py-3 px-2 lg:px-4 2xl:px-6 border-2 border-font-base lg:text-md 2xl:text-lg rounded-md flex items-center justify-center">
                 <span>User Experience</span>
               </div>
-              <div className="py-3 px-6 border-2 border-font-base text-lg rounded-md flex items-center justify-center">
+              <div className="hidden xl:flex py-1 lg:py-2 2xl:py-3 px-2 lg:px-4 2xl:px-6 border-2 border-font-base lg:text-md 2xl:text-lg rounded-md items-center justify-center">
                 <span>Fullstack</span>
               </div>
             </div>

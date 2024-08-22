2993dfcec3692038bca769a617ec5496e719aeb3
Pablo Fernandez <pfernandez@dataahead.de>
2024-08-22 18:25:44 +0200

    Use HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";

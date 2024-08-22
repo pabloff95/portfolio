8534b350940c3ccd363a4d3eb1d757e5d1275fcc
Pablo Fernandez <pfernandez@dataahead.de>
2024-08-22 20:28:43 +0200

    Improve responsive design for phone views

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -36,7 +36,7 @@ export default function Layout() {
   };
 
   return (
-    <div className="h-[100vh] w-[100vw] grid grid-rows-12 md:grid-cols-12 overflow-hidden">
+    <div className="h-[100vh] w-[100vw] flex flex-col md:grid md:grid-cols-12 overflow-y-hidden">
       {displayMenu && (
         <SideMenu
           setDisplayMenu={setDisplayMenu}
--- src/components/layout/Side-menu.tsx
+++ src/components/layout/Side-menu.tsx
@@ -14,16 +14,16 @@ const SideMenu: React.FC<SideMenuProps> = ({
   displayDarkMode,
 }) => {
   return (
-    <aside className="row-span-1 md:col-span-2 grid grid-cols-12 md:grid-cols-1 md:grid-rows-6 md:h-[100vh] w-[100vw] md:w-auto bg-secondary-background">
-      <section className="hidden min-[580px]:block row-span-1">
-        <div className="h-full font-impact flex flex-col md:justify-center text-center">
+    <aside className="flex justify-between md:col-span-2 md:grid md:grid-cols-1 md:grid-rows-6 md:h-[100vh] w-full md:w-auto bg-secondary-background">
+      <section className="hidden min-[580px]:block md:row-span-1">
+        <div className="h-full font-impact flex flex-col md:justify-center text-center pr-2 md:pr-0">
           <Logo />
           <p className="hidden lg:block text-[min(2vmax,1.35rem)]">
             Web developer
           </p>
         </div>
       </section>
-      <section className="col-span-11 min-[580px]:col-span-10 md:row-span-4 flex md:flex-col justify-between sm:justify-center items-center">
+      <section className="min-[580px]:col-span-10 md:row-span-4 flex grow md:flex-col sm:justify-center items-center">
         <MenuButton to="/" text="HOME" />
         <MenuButton to="/about" text="ABOUT" />
         <MenuButton to="/stack" text="STACK" />
